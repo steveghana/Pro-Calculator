9c6a0687c2638a407445af26293175a98ee26614
stephen Williams <82245766+steveghana@users.noreply.github.com>
2021-11-12 02:50:27 +0000

    first commit

---
+++ cal2.js
@@ -0,0 +1,201 @@
+let mainScreen = document.querySelector('#mainscreen')
+let numButtons = document.querySelector('#Number')
+let allInputs = document.querySelectorAll('input')
+let numScreen = document.getElementById('calcScreen')
+let answerBox = document.getElementById('Answerbox')
+let firstNum = []
+let secondNum = []
+let total1 = []
+let operator = []
+let total = []
+
+allInputs.forEach(input=>{
+    input.addEventListener('click', (e) => {
+
+
+        (function reset(clicked) {
+            if (clicked == 'AC') {
+                numScreen.innerText = 0
+                answerBox.innerText = ""
+                firstNum = []
+                secondNum = []
+                operator = []
+                total = []
+            }
+        })(input.value);
+
+
+        /////////////////
+        (function delet(clickedValue) {
+            if (clickedValue === "C") {
+                numScreen.innerText = numScreen.innerText.slice(0, -1)
+
+                if (numScreen.innerText.length == 0) {
+                    numScreen.innerText = 0
+                }
+
+            }
+        })(input.value);
+
+        
+        
+        
+        
+        ///////////////
+        (function InputConfig(clickedValue) {
+            if (numScreen.innerText == '0') {
+                //start up characaters
+                if (input.className == 'numbers' &&
+                    clickedValue != "." &&
+                    clickedValue != 'C' &&
+                    clickedValue != 'AC')
+                    numScreen.innerText = clickedValue;
+    
+            }
+            else {
+                if (input.className != 'operators' && clickedValue != '=' && clickedValue != 'C')
+                    numScreen.innerText += input.value
+    
+            }
+        })(input.value);
+
+
+        (function authenticateOperators(clickedValue) {
+
+    
+            if (input.className == 'operators') {
+
+                if (numScreen.innerText == '0' && firstNum.length == 0) {
+                    answerBox.innerText = ''
+                    firstNum = []
+              
+                } else {
+                    secondNum = []
+           
+                    let operate;
+                    if (clickedValue === 'x') {
+                        operator.push('*')
+                        operate = clickedValue
+                    } else {
+                        operator.push(clickedValue)
+                        operate = operator[operator.length - 1]
+                    }
+
+
+                    if (firstNum.length == 0) {
+                        firstNum.push(parseFloat(numScreen.innerText)) //first num to use for resolving
+            
+                        numScreen.innerText = '0'
+        
+                    }
+                    if (operator.length == 3) { //to control the operator array when we overclick or click more than once
+                        operator.shift()//new operators are pushed to the end, so previous ones shold be removed (shift)
+                    }
+                    answerBox.innerText = firstNum[0] + ' ' + operate
+                }
+            }
+        })(input.value);
+
+
+
+
+
+        (function customiZero(){
+            if (input.value == '0' && firstNum.length != 0) {
+                secondNum = []
+                if (numScreen.innerText == '0') {
+                    numScreen.innerText = input.value
+                
+                }
+                secondNum.push(parseFloat(input.value))
+   
+            }
+        })();
+
+
+
+
+
+
+////////
+(function resolveOnOperators(){
+ 
+    if(input.className === 'operators' && firstNum.length !=0 && numScreen.innerText != '0'){
+        secondNum = []
+        if (input.value == '0') {
+            numScreen.innerText = input.value
+            secondNum.push(parseFloat(numScreen.innerText))
+            
+        } else {
+            secondNum.push(parseFloat(numScreen.innerText))
+            
+        }
+            
+            let resolved = `${firstNum[0]}${operator[operator.length - 2]}${secondNum[0]}`
+            let resolve  =  eval(resolved)
+             answerBox.innerText = resolve + ' ' + input.value
+             firstNum = []
+            firstNum.push(resolve)
+    numScreen.innerText = '0'
+
+    }
+    
+})();
+  
+//UTILITY FUNCTION
+        function customizedResolve(index) {
+            let res = `${parseFloat (firstNum[0])}${operator[operator.length - index]}${parseFloat(secondNum[0])}`
+            let resolve = eval(res)
+            numScreen.innerText = resolve
+            answerBox.innerText = ''
+}
+
+//  //////////
+        
+        
+        
+(function resolve(){
+   
+if( input.value == '=' ){
+   
+    if(firstNum == [] && secondNum == [] ){
+        total.push(numScreen.innerText)
+        
+    }
+   
+  
+   
+    if (secondNum.length == 1) {
+       //we dont need second num more than once
+       secondNum = []
+       secondNum.push(numScreen.innerText)
+   customizedResolve(1)
+
+   }
+
+    
+   if(secondNum.length == 0 && firstNum.length !=0){
+
+       if (numScreen.innerText != '0') {
+        secondNum.push(parseFloat(numScreen.innerText)) 
+   customizedResolve(1)
+    
+       } else {
+           secondNum = []
+           numScreen.innerText = firstNum
+      answerBox.innerText = ""
+
+      }
+    
+   
+   }
+   firstNum = []
+    secondNum = []
+    
+}
+})();
+
+        })
+    })
+
+
